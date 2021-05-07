9cfbd1cee39aef63ffcfd9f2d9fb31442dcad13d
chalnicol <chalnicol@gmail.com>
2021-05-07 15:15:02 +0800

    Change in kinge

--- client/assets/js/proper.js
+++ client/assets/js/proper.js
@@ -5,8 +5,13 @@ class Proper extends Phaser.Scene {
         super('Proper');
     }
 
-    create () {
+    create ( data ) {
+
+
         this.add.rectangle ( 100, 100, 100, 100, 0xffff00, 1 );
+
+        this.add.text ( 100, 100, data.mode, { color:'#333', fontFamily:'Oswald', fontSize:50 }).setOrigin (0.5);
+        
     }
 
     
